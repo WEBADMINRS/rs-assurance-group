c7cab0e3dfe6241edfb21347506a71d108244a04
Kenneth Raneses <kennethraneses@Kenneths-MacBook-Pro.local>
2026-07-31 11:29:04 +1000

    Initial RS Assurance Group setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>RS Assurance Group</h1>
+      <p>Building organisations that stand up to scrutiny.</p>
+    </main>
+  );
+}
+
+export default App;
